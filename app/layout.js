60190502375d9247729638468c800b882cc0f907
Shivam Tiwari <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2024-08-18 02:39:06 +0530

    push

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <head>
+        {/* Preconnect to Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        
+        {/* Link to external fonts */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Annapurna+SIL:wght@400;700&family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=Martel+Sans:wght@200;300;400;600;700;800;900&family=Martel:wght@200;300;400;600;700;800;900&family=Yatra+One&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      
+      <body className={inter.className}>
         <Navbar /> {/* Global Navbar */}
         <main>{children}</main> {/* Main content area */}
         <Footer /> {/* Global Footer */}
