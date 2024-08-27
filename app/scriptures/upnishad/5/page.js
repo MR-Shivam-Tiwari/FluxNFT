dadb57828f6853bbfb2c601128b8f73b75c449f0
Shivam Tiwari <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2024-08-27 17:50:15 +0530

    push

--- app/scriptures/upnishad/5/page.js
+++ app/scriptures/upnishad/5/page.js
@@ -36,7 +36,7 @@ function page() {
                                     {/* <a className="inline-flex items-center justify-center rounded-md bg-blue-500 hover:bg-blue-600 text-white font-medium px-6  transition-colors"  >
               Purchase
             </a> */}
-                                    <Link shallow href='/scriptures/geeta/viewbhagvad/bhagvadgeeta'>
+                                    <Link shallow href='/scriptures/upnishad/5/mandukya'>
                                         <button
                                             className="inline-flex gap-3  items-center w-[300px] bg-orange-500 hover:bg-orange-400 border-orange-300 shadow justify-center whitespace-nowrap text-xl  font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-11 rounded-md px-8"
                                         >
