8723baa631d31c9add8752b41d614ebd51f7b792
Shivam Tiwari <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2024-12-12 20:50:58 +0530

    push

--- app/layout.js
+++ app/layout.js
@@ -29,12 +29,49 @@ export default function RootLayout({ children }) {
         />
 
         {/* Favicon */}
-        <link className='' rel="icon" type="image/png" href="/homeimage/shashtrasangrahlogo.png" />
-        {/* <link rel="alternate icon" type="image/png" href="/favicon.ico" /> */}
+        <link rel="icon" type="image/png" href="/homeimage/shashtrasangrahlogo.png" />
 
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WLLG8JXF');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
+        {/* Google Analytics (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-F08EGV49EE"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-F08EGV49EE');
+            `,
+          }}
+        />
+        {/* End Google Analytics */}
       </head>
 
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WLLG8JXF"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <Navbar /> {/* Global Navbar */}
         <main className="pt-8">{children}</main> {/* Main content area */}
         {/* <Footer /> */}
