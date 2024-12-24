9beb78e95c209f33afd9d34152b0d2c50028eb27
Shivam Tiwari <114647321+MR-Shivam-Tiwari@users.noreply.github.com>
2024-12-24 17:42:15 +0530

    push

--- app/page.js
+++ app/page.js
@@ -57,7 +57,7 @@ const Home = () => {
                       className="inline-flex items-center w-full shadow-xl hover:scale-105 hover:shadow-2xl  transform duration-500 lg:w-[250px] justify-center cursor-pointer px-2.5 py-2 border text-lg  border-gray-300  font-bold rounded-md text-gray-700 bg-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     // onClick={() => router.push("/donation")}
                     >
-                      Support The Developer
+                      Donate to Empower Us
                     </div>
                   </Link>
                 </div>
